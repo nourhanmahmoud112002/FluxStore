333a621bb6b6cd1bcddafb9826d91bf199c2744f
Nourhan Mahmoud <nourhanmahmoudsalem@gmail.com>
2026-02-21 17:02:51 +0200

    feat: refactor storage module to improve data handling and simplify storage access

--- src/common/storage/index.ts
+++ src/common/storage/index.ts
@@ -1,7 +1,10 @@
-import { createMMKV} from 'react-native-mmkv';
-const getStorage = createMMKV;
+import { createMMKV } from 'react-native-mmkv';
+
+const storage = createMMKV();
+
 export const MMKV_KEYS = {
   TRANSLATION_KEY: 'app_language',
+  USER_KEY: 'user_data',
 } as const;
 
 export const APP_LANGUAGES = {
@@ -9,14 +12,44 @@ export const APP_LANGUAGES = {
   AR: 'ar',
 } as const;
 
-export const storeData = (key: string, value: string) => {
-  getStorage().set(key, value);
+export const storeData = (
+  key: string,
+  value: string | number | boolean | object,
+) => {
+  if (typeof value === 'object') {
+    storage.set(key, JSON.stringify(value));
+  } else {
+    storage.set(key, value);
+  }
+};
+
+export const getStringData = (key: string): string | undefined => {
+  return storage.getString(key);
+};
+
+export const getNumberData = (key: string): number | undefined => {
+  return storage.getNumber(key);
+};
+
+export const getBooleanData = (key: string): boolean | undefined => {
+  return storage.getBoolean(key);
+};
+
+export const getObjectData = <T>(key: string): T | undefined => {
+  const value = storage.getString(key);
+  try {
+    return value ? JSON.parse(value) : undefined;
+  } catch (e) {
+    console.log(e);
+
+    return undefined;
+  }
 };
 
 export const getData = (key: string): string | undefined => {
-  return getStorage().getString(key);
+  return storage.getString(key);
 };
 
 export const removeData = (key: string) => {
-  getStorage().remove(key);
+  storage.remove(key);
 };
